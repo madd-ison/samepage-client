3fd7da928ce4aab4ebfc6643f3055a8dc3346e8d
Maddison Murphy <maddison@puppyteeth.org>
2020-10-25 19:26:38 -0400

    initial deploy

---
+++ src/App/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders the App without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+});
