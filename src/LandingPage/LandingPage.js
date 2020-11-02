fc5ba267b033eedf9d5f211eeb6dd69d55ba79c8
Maddison Murphy <maddison@puppyteeth.org>
2020-11-01 19:50:59 -0500

    update buttons

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -9,8 +9,10 @@ function LandingPage() {
                 <h2>Make sure you're on the</h2>
                 <h1 id='samepage'>samePage.</h1>
                     <h4>Free & easy. Start your group discussion now:</h4>
-                    <Link to='/signup'><button>START YOUR PAGE</button></Link>
-                    <Link to='/login'><button id='login-btn'>Log In</button></Link>
+                    <div id='links'>
+                        <Link to='/signup'><div id='signup-btn'>START YOUR PAGE</div></Link>
+                        <Link to='/login'><div id='login-btn'>LOG IN</div></Link>
+                    </div>
                     <section id='example'>
                         <div id='ex-text'>
                             <h1>Projects, vacations, evenings, <i>anything</i>.</h1>
